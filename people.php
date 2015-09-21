fbe7044f9515820a6d18203c7121a1d6ccb78c5f
Nathan Hare <nhare330@gmail.com>
2015-09-21 11:28:45 -0400

    Clean up echo commands in profile and people

--- people.php
+++ people.php
@@ -37,72 +37,90 @@
   $result = mysql_query($sql);
 
 
-?>
+  echo "
 
-<h2>People</h2>
+    <h2>People</h2>
 
-<!--- ###################### SEARCH BAR ##################### --->
+    <!--- ###################### SEARCH BAR ##################### --->
 
-<div class="greenbox" style="text-align: center;" >
-  <form method="post" action="?page=people">
-    <input type="text" name="search" value="<?php echo $search; ?>" size=75% autofocus /><input type="submit" value="Search" /><br /><br />
-    <label>Canonsburg, PA <input type="checkbox" name="canonsburg" value="canonsburg" onchange="this.form.submit()" <?php if (in_array("canonsburg", $location)) { echo 'checked'; } ?> /></label>&nbsp;&nbsp;&nbsp;&nbsp;
-    <label>Columbia, MD <input type="checkbox" name="columbia" value="columbia" onchange="this.form.submit()" <?php if (in_array("columbia", $location)) { echo 'checked'; } ?> /></label>&nbsp;&nbsp;&nbsp;&nbsp;
-    <label>Hershey, PA <input type="checkbox" name="hershey" value="hershey" onchange="this.form.submit()" <?php if (in_array("hershey", $location, TRUE)) { echo 'checked'; } ?> /></label>&nbsp;&nbsp;&nbsp;&nbsp;
-    <label>State College, PA <input type="checkbox" name="statecollege" value="state college" onchange="this.form.submit()"<?php if (in_array("state college", $location)) { echo 'checked'; } ?> /></label>&nbsp;&nbsp;&nbsp;&nbsp;
-    <label>Wilkes-Barre, PA <input type="checkbox" name="wilkesbarre" value="wilkes-barre" onchange="this.form.submit()" <?php if (in_array("wilkes-barre", $location)) { echo 'checked'; } ?> /></label>
-  </form> 
-</div>
+    <div class='greenbox' style='text-align: center;' >
+      <form method='post' action='?page=people'>
+        <input type='text' name='search' value='$search' size=75% autofocus /><input type='submit' value='Search' /><br /><br />
+        <label>Canonsburg, PA <input type='checkbox' name='canonsburg' value='canonsburg' onchange='this.form.submit()'";      
+          if (in_array("canonsburg", $location)) { echo 'checked'; } 
+          echo " /></label>&nbsp;&nbsp;&nbsp;&nbsp;
+        <label>Columbia, MD <input type='checkbox' name='columbia' value='columbia' onchange='this.form.submit()'"; 
+          if (in_array("columbia", $location)) { echo 'checked'; } 
+          echo " /></label>&nbsp;&nbsp;&nbsp;&nbsp;
+        <label>Hershey, PA <input type='checkbox' name='hershey' value='hershey' onchange='this.form.submit()'"; 
+          if (in_array("hershey", $location, TRUE)) { echo 'checked'; } 
+          echo " /></label>&nbsp;&nbsp;&nbsp;&nbsp;
+        <label>State College, PA <input type='checkbox' name='statecollege' value='state college' onchange='this.form.submit()'"; 
+          if (in_array("state college", $location)) { echo 'checked'; } 
+          echo " /></label>&nbsp;&nbsp;&nbsp;&nbsp;
+        <label>Wilkes-Barre, PA <input type='checkbox' name='wilkesbarre' value='wilkes-barre' onchange='this.form.submit()'"; 
+          if (in_array("wilkes-barre", $location)) { echo 'checked'; } 
+          echo " /></label>
+      </form> 
+    </div>
 
-<br />
-<br />
-
-<?php 
+    <br />
+    <br />
+  ";
 
   multipageNav($limit, $total_rows);
+
+   /////////////////////// 
+  //    PEOPLE LIST    //
+ ///////////////////////
 
   if ($result) {
     $count = 0;
     while ($row = mysql_fetch_array($result)) {
       $count ++;
-?>
 
-<div class="people-list" style="height: 80px;<?php if ($count % 2 == 0) { echo 'background: #dedede;'; } ?>">
-<a href="?page=profile&profileid=<?php echo $row['id']; ?>">
-  <img src="<?php if ($row['picture']) { echo $row['picture']; } else { ?>img/no_pic1.png<?php } ?>" width="75" />
-</a> 
-<table width=50% class="people-info">
-  <tr>
-    <td width=50% align="right">
-      <b>Phone:</b>
-    </td>
-    <td width=50%>
-      <?php echo $row['phone']; ?>
-    </td>
-  </tr>
-  <tr>
-    <td align="right">
-      <b>Extenstion:</b>
-    </th>
-    <td>
-      <?php echo $row['ext']; ?>
-    </td>
-  </tr>
-  <tr>
-    <td align="right">
-      <b>Email:</b>
-    </td>
-    <td>
-      <a href="mailto:<?php echo $row['email']; ?>"><?php echo $row['email']; ?></a>
-    </td>
-  </tr>
-</table>
-<a href="?page=profile&profileid=<?php echo $row['id']; ?>"><b><?php echo $row['fname'] . " " . $row['lname']; ?></b></a>
-<p><?php echo $row['title']; ?><br />
-<?php echo $row['location']; ?></p>
-</div>
+      echo "
 
-<?php 
+        <div class='people-list' style='height: 80px;";
+          if ($count % 2 == 0) { echo 'background: #dedede;'; } 
+          echo "'>
+        <a href='?page=profile&profileid=$row[id]'>
+          <img src='";
+            if ($row['picture']) { echo $row['picture']; } else { echo "img/no_pic1.png"; } 
+            echo "' width='75' />
+        </a> 
+        <table width=50% class='people-info'>
+          <tr>
+            <td width=50% align='right'>
+              <b>Phone:</b>
+            </td>
+            <td width=50%>
+              $row[phone]
+            </td>
+          </tr>
+          <tr>
+            <td align='right'>
+              <b>Extenstion:</b>
+            </th>
+            <td>
+              $row[ext]
+            </td>
+          </tr>
+          <tr>
+            <td align='right'>
+              <b>Email:</b>
+            </td>
+            <td>
+              <a href='mailto:$row[email]'>$row[email]</a>
+            </td>
+          </tr>
+        </table>
+        <a href='?page=profile&profileid=$row[id]'><b>$row[fname] $row[lname]</b></a>
+        <p>$row[title]<br />
+        $row[location]</p>
+        </div>
+      ";
+
     }
   } else {
     echo mysql_error();
@@ -110,136 +128,3 @@
   echo "<br />";
   multipageNav($limit, $total_rows);
 ?>
-
-<!----
-
-<div class="people-list">
-<a href="?page=profile&profileid=3"><img src="img/no_pic1.png" width="75" /></a> 
-<table width=50% class="people-info">
-  <tr>
-    <td width=50% align="right">
-      <b>Phone:</b>
-    </td>
-    <td width=50%>
-      717-508-0563
-    </td>
-  </tr>
-  <tr>
-    <td align="right">
-      <b>Extenstion:</b>
-    </th>
-    <td>
-      x1159
-    </td>
-  </tr>
-  <tr>
-    <td align="right">
-      <b>Email:</b>
-    </td>
-    <td>
-      <a href="mailto:jkreider@armgroup.net">jkreider@armgroup.net</a>
-    </td>
-  </tr>
-</table>
-<a href="?page=profile&profileid=3"><b>Josh Kreider</b></a>
-<p>IT Administrator<br />
-Hershey, PA</p>
-</div>
-<div class="people-list" style="background: #dedede;">
-<a href="?page=profile&profileid=1"><img src="img/nate.jpg" width="75" /></a> 
-<table width=50% class="people-info">
-  <tr>
-    <td width=50% align="right">
-      <b>Phone:</b>
-    </td>
-    <td width=50%>
-      717-781-0268
-    </td>
-  </tr>
-  <tr>
-    <td align="right">
-      <b>Extenstion:</b>
-    </th>
-    <td>
-      x1162
-    </td>
-  </tr>
-  <tr>
-    <td align="right">
-      <b>Email:</b>
-    </td>
-    <td>
-      <a href="mailto:nhare@armgroup.net">nhare@armgroup.net</a>
-    </td>
-  </tr>
-</table>
-<a href="?page=profile&profileid=nhare"><b>Nathan Hare</b></a>
-<p>Help Desk Analyst<br />
-Hershey, PA</p>
-</div>
-<div class="people-list">
-<a href="?page=profile&profileid=jkreider"><img src="img/no_pic1.png" width="75" /></a> 
-<table width=50% class="people-info">
-  <tr>
-    <td width=50% align="right">
-      <b>Phone:</b>
-    </td>
-    <td width=50%>
-      717-508-0563
-    </td>
-  </tr>
-  <tr>
-    <td align="right">
-      <b>Extenstion:</b>
-    </th>
-    <td>
-      x1159
-    </td>
-  </tr>
-  <tr>
-    <td align="right">
-      <b>Email:</b>
-    </td>
-    <td>
-      <a href="mailto:jkreider@armgroup.net">jkreider@armgroup.net</a>
-    </td>
-  </tr>
-</table>
-<a href="?page=profile&profileid=3"><b>Josh Kreider</b></a>
-<p>IT Administrator<br />
-Hershey, PA</p>
-</div>
-<div class="people-list" style="background: #dedede;">
-<a href="?page=profile&profileid=1"><img src="img/nate.jpg" width="75" /></a> 
-<table width=50% class="people-info">
-  <tr>
-    <td width=50% align="right">
-      <b>Phone:</b>
-    </td>
-    <td width=50%>
-      717-781-0268
-    </td>
-  </tr>
-  <tr>
-    <td align="right">
-      <b>Extenstion:</b>
-    </th>
-    <td>
-      x1162
-    </td>
-  </tr>
-  <tr>
-    <td align="right">
-      <b>Email:</b>
-    </td>
-    <td>
-      <a href="mailto:nhare@armgroup.net">nhare@armgroup.net</a>
-    </td>
-  </tr>
-</table>
-<a href="?page=profile&profileid=1"><b>Nathan Hare</b></a>
-<p>Help Desk Analyst<br />
-Hershey, PA</p>
-</div>
-
----->
--- profile.php
+++ profile.php
@@ -12,101 +12,125 @@
   if (isset($_POST['action'])) {                    /* ADD or EDIT EMPLOYEE */
     $action = $_POST['action'];
     
-    echo '<form method="post" action="?page=profile&profileid=' . $profileid . '">';
-    if ($action == 'edit') { echo '<h2>Edit Profile</h2>'; }else{ echo '<h2>Add New Employee</h2>'; }
-    echo '<table width=100%>';
-      echo '<tr>';
-        echo '<td width="160" valign="top">  <!----PICTURE---->';
-          if ($row['picture']) { 
-            echo '<img src="' . $row['picture'] . '" width="150" />'; 
-          } else { 
-            echo '<img src="img/no_pic1.png" width="150" />'; }
-          echo "<br /><br /><input type='file' style='width: 175px;' accept='image/*' name='picture' />";
-        echo '</td>';
-        echo '<td valign="top">  <!----INFORMATION---->';
-          echo '<div style="margin-left: 0px; margin-right: 75px; width=300px; text-align: right;line-height: 27px;"><b>';
-            echo 'First Name: <input type="text" name="fname" value="' . $row['fname'] . '" /><br />';
-            echo 'Last Name: <input type="text" name="lname" value="' . $row['lname'] . '" /><br />';
-            echo 'Initials: <input type="text" name="initials" value="' . $row['initials'] . '" /><br />';
-            echo 'Title: <input type="text" name="title" value="' . $row['title'] . '" /><br />';
-            echo 'Office:&nbsp;&nbsp;&nbsp; <select name="location">';
-              echo "<option default>Choose one...</option>";
-              echo "<option value='Canonsburg, PA'"; if ($row['location'] == 'Canonsburg, PA') { echo " selected "; } echo ">Canonsburg, PA</option>";
-              echo "<option value='Columbia, MD'"; if ($row['location'] == 'Columbia, MD') { echo " selected "; } echo ">Columbia, MD</option>";
-              echo "<option value='Hershey, PA'"; if ($row['location'] == 'Hershey, PA') { echo " selected "; } echo ">Hershey, PA</option>";
-              echo "<option value='State College, PA'"; if ($row['location'] == 'State College, PA') { echo " selected "; } echo ">State College, PA</option>";
-              echo "<option value='Wilkes-Barre, PA'"; if ($row['location'] == 'Wilkes-Barre, PA') { echo " selected "; } echo ">Wilkes-Barre, PA</option></select><br />";
-            echo 'Email: <input type="text" name="email" value="' . $row['email'] . '" /><br />';
-            echo 'Phone: <input type="text" name="phone" value="' . $row['phone'] . '" /><br />';
-            echo 'Extension: <input type="text" name="ext" value="' . $row['ext'] . '" /><br />';
-            echo 'Start Day: <input type="text" id="datepicker" name="startday" value="' . date('Y/m/d', strtotime($row['startday'])) . '" /><br />';
-            echo 'Birthday: <input type="text" id="datepicker2" name="birthday" value="' . date('Y/m/d', strtotime($row['birthday'])) . '" /><br />';
-            echo 'Show Birthday: <input type="checkbox" name="showbday" value="y"';
-            if ($row['showbday'] == 'y') { echo ' checked'; }
-            echo ' /><br />';
-          echo '</b></div>';
-        echo '</td>';
-        echo  '<td valign="top" width=35%>  <!----ABOUT---->';
-          echo '<div class="profile-about">';
-            echo '<h3>Areas of expertise...</h3>';
-            echo '<textarea cols="36" rows="5" name="description" style="padding: 5px;">' .  $row['description'] . '</textarea>';
-          echo '</div>';
-          echo '<div class="profile-about">';
-            echo '<h3>A bit about me...</h3>';
-            echo '<textarea cols="36" rows="5" name="about" style="padding: 5px;">' . $row['about'] . '</textarea>';
-          echo '</div>';
-        echo '</td>';
-      echo '</tr>';
-    echo '</table>';
-    echo '<input type="hidden" name="submit" value="' . $action . '" />';
-    echo '<input type="hidden" name="type" value="employee" />';
-    echo '<center><input type="submit" value="Submit" style="margin-left: auto; margin-right: auto;" /></center>';
-    echo '</form>';
+    echo "<form method='post' action='?page=profile&profileid=$profileid'>";
+    if ($action == 'edit') { echo "<h2>Edit Profile</h2>"; }else{ echo "<h2>Add New Employee</h2>"; }
+    echo "
+      <table width=100%>
+        <tr>
+          <td width='160' valign='top'>  <!----PICTURE---->";
+            if ($row['picture']) { 
+              echo "<img src='$row[picture]' width='150' />"; 
+            } else { 
+              echo "<img src='img/no_pic1.png' width='150' />"; }
+    echo "
+            <br /><br /><input type='file' style='width: 175px;' accept='image/*' name='picture' />
+          </td>
+          <td valign='top'>  <!----INFORMATION---->
+            <div style='margin-left: 0px; margin-right: 75px; width=300px; text-align: right;line-height: 27px;'><b>
+              First Name: <input type='text' name='fname' value='$row[fname]' /><br />
+              Last Name: <input type='text' name='lname' value='$row[lname]' /><br />
+              Initials: <input type='text' name='initials' value='$row[initials]' /><br />
+              Title: <input type='text' name='title' value='$row[title]' /><br />
+              Office:&nbsp;&nbsp;&nbsp; <select name='location'>
+                <option default>Choose one...</option>
+                <option value='Canonsburg, PA'"; 
+                  if ($row['location'] == 'Canonsburg, PA') { echo " selected "; } 
+                  echo ">Canonsburg, PA</option>
+                <option value='Columbia, MD'"; 
+                  if ($row['location'] == 'Columbia, MD') { echo " selected "; } 
+                  echo ">Columbia, MD</option>
+                <option value='Hershey, PA'"; 
+                  if ($row['location'] == 'Hershey, PA') { echo " selected "; } 
+                  echo ">Hershey, PA</option>
+                <option value='State College, PA'"; 
+                  if ($row['location'] == 'State College, PA') { echo " selected "; } 
+                  echo ">State College, PA</option>
+                <option value='Wilkes-Barre, PA'"; 
+                  if ($row['location'] == 'Wilkes-Barre, PA') { echo " selected "; } 
+                  echo ">Wilkes-Barre, PA</option></select><br />
+              Email: <input type='text' name='email' value='$row[email]' /><br />
+              Phone: <input type='text' name='phone' value='$row[phone]' /><br />
+              Extension: <input type='text' name='ext' value='$row[ext]' /><br />
+              Start Day: <input type='text' id='datepicker' name='startday' value='" . date('Y/m/d', strtotime($row['startday'])) . "' /><br />
+              Birthday: <input type='text' id='datepicker2' name='birthday' value='" . date('Y/m/d', strtotime($row['birthday'])) . "' /><br />
+              Show Birthday: <input type='checkbox' name='showbday' value='y'";
+                if ($row['showbday'] == 'y') { echo ' checked'; }
+                echo " /><br />
+              </b></div>
+            </td>
+            <td valign='top' width=35%>  <!----ABOUT---->
+              <div class='profile-about'>
+                <h3>Areas of expertise...</h3>
+                <textarea cols='36' rows='5' name='description' style='padding: 5px;'>$row[description]</textarea>
+              </div>
+              <div class='profile-about'>
+                <h3>A bit about me...</h3>
+                <textarea cols='36' rows='5' name='about' style='padding: 5px;'>$row[about]</textarea>
+              </div>
+            </td>
+          </tr>
+        </table>
+        <input type='hidden' name='submit' value='$action' />
+        <input type='hidden' name='type' value='employee' />
+        <center><input type='submit' value='Submit' style='margin-left: auto; margin-right: auto;' /></center>
+      </form>
+    ";
   
   } else {
     
     if (mysql_num_rows($result) == 1) {
       if ( hasPermission('admin') || $_SESSION['id'] == $row['id'] ) {                  /* VIEW PROFILE */
-?>
 
-      <form method="post" action="?page=profile&profileid=<?php echo $row['id']; ?>">
-        <input type="hidden" name="action" value="edit" />
-        <input style="float: right;" type="submit" value="Edit Profile" />
-      </form>
-    <?php } ?> 
-   <h2><?php echo $row['fname'] . ' ' . $row['lname']; ?></h2>
-    <table width=100%>
-      <tr>
-        <td width="160" valign="top">  <!----PICTURE---->
-          <img src="<?php if ($row['picture']) { echo $row['picture']; } else { echo 'img/no_pic1.png'; } ?>" width="150" />
-        </td>
-        <td valign="top">  <!----INFORMATION---->
-          <b><?php echo $row['fname'] . ' ' . $row['lname']; ?></b> (<?php echo $row['initials']; ?>)<br />
-          <?php echo $row['title']; ?><br />
-          <?php if ($row['location']) { echo $row['location'] . '<br />'; } ?><br />
-          <a href="mailto:<?php echo $row['email']; ?>"><?php echo $row['email']; ?></a><br />
-          <?php if ($row['phone'] && $row['ext']) {echo $row['phone'] . ' x' . $row['ext'];} ?><br /><br />
-          <?php if ($row['startday']) { ?><u>Start Date:</u> <?php echo date('F j, Y', strtotime($row['startday'])); } ?><br />
-          <?php if ($row['birthday'] && $row['showbday'] == 'y') { ?><u>Birthday:</u> <?php echo date('F j', strtotime($row['birthday'])); } ?>
-        </td>
-        <td valign="top" width=35%>  <!----ABOUT---->
-          <div class="profile-about">
-            <h3>Areas of Expertise...</h3>
-            <p style="padding: 5px;"><?php echo $row['description']; ?></p>
-          </div>
-          <div class="profile-about">
-            <h3>A bit about me...</h3>
-            <p style="padding: 5px;"><?php echo $row['about']; ?></p>
-          </div>
+        echo "
+          <form method='post' action='?page=profile&profileid=$row[id]'>
+            <input type='hidden' name='action' value='edit' />
+            <input style='float: right;' type='submit' value='Edit Profile' />
+          </form>
+        ";
+      }
+      
+      echo "
+       <h2>$row[fname] $row[lname]</h2>
+        <table width=100%>
+          <tr>
+            <td width='160' valign='top'>  <!----PICTURE---->
+              <img src=";
+                if ($row['picture']) { echo $row['picture']; } else { echo 'img/no_pic1.png'; } 
+                echo " width='150' />
+            </td>
+            <td valign='top'>  <!----INFORMATION---->
+              <b>$row[fname] $row[lname]</b> ($row[initials])<br />
+              $row[title]<br />";
+              if ($row['location']) { echo $row['location'] . '<br />'; } 
+                echo "<br />
+              <a href='mailto:$row[email]'>$row[email]</a><br />";
+              if ($row ['phone'] && $row['ext']) {echo $row['phone'] . ' x' . $row['ext'];} 
+                echo "<br /><br />";
+              if ($row['startday']) { echo "<u>Start Date:</u>" . date('F j, Y', strtotime($row['startday'])); } 
+                echo "<br />";
+              if ($row['birthday'] && $row['showbday'] == 'y') { echo "<u>Birthday:</u>" . date('F j', strtotime($row['birthday'])); }
+                echo "
+            </td>
+            <td valign='top' width=35%>  <!----ABOUT---->
+              <div class='profile-about'>
+                <h3>Areas of Expertise...</h3>
+                <p style='padding: 5px;'>$row[description]</p>
+              </div>
+              <div class='profile-about'>
+                <h3>A bit about me...</h3>
+                <p style='padding: 5px;'>$row[about]</p>
+              </div>
+            </td>
+          </tr>
+        </table>
+      ";
 
-        </td>
-      </tr>
-    </table>
   
-<?php   
     } else {
-      echo "<h2>Profiles</h2>";
-      echo "Profile not found!";
+      echo "
+        <h2>Profiles</h2>
+        Profile not found!
+      ";
     }
   }
 ?>
