79b11b82d05d28c681f0d3949c6a774e21fa5566
Nathan Hare <nhare330@gmail.com>
2015-11-03 13:40:05 -0500

    Correct required location bug for all articles and only display active areas of expertise

--- expertise.php
+++ expertise.php
@@ -9,19 +9,19 @@
 
   foreach ($expertise as $cat) {
 
-    $identifier = preg_replace("/[^a-zA-Z0-9]/", "", $cat);
-
-    echo "
-      <div class='togglebtn' onClick='hidediv(\"$identifier\")' id='$identifier-btn'><span id='$identifier-arw' style='float:right;font-weight:normal;'>&#9650; &nbsp;&nbsp;</span>&nbsp;&nbsp;$cat</div>
-      <div class='togglediv' id='$identifier'>
-        <ul>
-          <br />
-    ";
-
     $sql = "SELECT * FROM staff WHERE description LIKE '%$cat%' ORDER BY lname";
     $result = mysql_query($sql);
     
     if (mysql_num_rows($result)) {
+      $identifier = preg_replace("/[^a-zA-Z0-9]/", "", $cat);
+
+      echo "
+        <div class='togglebtn' onClick='hidediv(\"$identifier\")' id='$identifier-btn'><span id='$identifier-arw' style='float:right;font-weight:normal;'>&#9650; &nbsp;&nbsp;</span>&nbsp;&nbsp;$cat</div>
+        <div class='togglediv' id='$identifier'>
+          <ul>
+            <br />
+      ";
+
 
       while ($staff = mysql_fetch_array($result)) {
         echo "
@@ -30,7 +30,7 @@
       }
 
     } else {
-      echo "<li>None found</li><br />";
+      //echo "<li>None found</li><br />";
     }
 
     echo "
